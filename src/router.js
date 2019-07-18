efe63aed54a463042f53ab95d7167b6a90a24835
Ilich Arredondo <ilichh1@hotmail.com>
2019-07-18 09:01:01 -0500

    Registered vue routes

---
+++ src/router.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// Views
+import Home from '@/views/HomeView.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: function () { 
+        return import(/* webpackChunkName: "about" */ '@/views/LoginView.vue')
+      }
+    },
+    {
+      path: '/states',
+      name: 'states-listing',
+      component: function () { 
+        return import(/* webpackChunkName: "listing" */ '@/views/StatesListingView.vue')
+      }
+    }
+  ]
+})
