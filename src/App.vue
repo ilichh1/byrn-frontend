882883f66d21408807315d1391727863e89a9c36
Ilich Arredondo <ilichh1@hotmail.com>
2019-07-18 08:02:55 -0500

    Created login component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <main>
-    <HomeComponent/>
+    <HeaderComponent/>
+    <!--HomeComponent/-->
+    <LoginComponent/>
+    <FooterComponent/>
   </main>
 </template>
 
 <script>
-import HomeComponent from './components/HomeComponent'
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+
+import HomeComponent from './components/root-components/HomeComponent';
+import LoginComponent from './components/root-components/LoginComponent';
 
 export default {
   name: 'app',
   components: {
-    HomeComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   }
 }
 </script>
