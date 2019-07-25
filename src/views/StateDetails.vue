8327d5548201150ece64bdf05a58b2d93d689019
Ilich Arredondo <ilichh1@hotmail.com>
2019-07-24 21:23:41 -0500

    [WIP] Detalle de propiedad

--- src/views/StateDetails.vue
+++ src/views/StateDetails.vue
@@ -1,5 +1,72 @@
 <template>
-<div class="site-wrap">
+<div class="site-wrap bg-secondary">
+  <div
+    class="site-blocks-cover inner-page-cover overlay"
+    style="background-image: url(images/summer-grass.jpg);"
+    data-aos="fade"
+    data-stellar-background-ratio="0.5">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-12 text-center">
+          <h1 class="text-white">PROPIEDAD</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <section class="container py-4" id="estate-detail">
+    <div class="row">
+      <div class="col-md-8">
+        <h2 class="text-white">Titulo de la propiedad</h2>
+        <hr>
+        <div class="owl-carousel nonloop-block-13">
+          <img
+            v-for="(image, idx) in estateImages"
+            :key="`imagen-${idx}`"
+            :src="image.src" :alt="image.desc">
+        </div>
+        <div class="border border-dark rounded mt-3 p-3 bg-white">
+          <p class="h4 mb-2 text-black">Descripcion del terreno </p>
+          <p class="mb-1 text-black">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consectetur unde reprehenderit
+            aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt
+            consequatur. Amet dolores excepturi earum unde iusto
+          </p>
+        </div>
+      </div>
+
+      <div class="col-md-4">
+        <h2 class="text-white mb-1">Contacta al anunciante</h2>
+        <form class="p-3 bg-white">
+          <div class="row form-group">
+            <div class="col-md-12">
+              <label class="text-black" for="email">Correo</label>
+              <input type="email" id="email" class="form-control">
+            </div>
+          </div>
+          <div class="row form-group">
+            <div class="col-md-12">
+              <label class="text-black" for="phone">Teléfono</label>
+              <input type="text" id="phone" class="form-control">
+            </div>
+          </div>
+          <div class="row form-group">
+            <div class="col-md-12 ">
+              <label class="text-black" for="message">Mensaje</label>
+              <textarea rows="6" class="form-control"></textarea>
+            </div>
+          </div>
+          <div class="row form-group">
+            <div class="col-md-12">
+              <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+</div>
+  <!--div class="site-wrap">
     <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(images/summer-grass.jpg);"
       data-aos="fade" data-stellar-background-ratio="0.5">
       <div class="container">
@@ -9,26 +76,27 @@
               <div class="col-md-8 text-left">
                 <h1>Titulo de Terreno</h1>
                 <hr style="background:white">
-                  <form action="#" class="p-5 bg-white" >
-                        <div class="col-md-12">
-                          <label class=" mb-5 text-black">CARRUSEL</label>
-                        </div>
-                  </form>
-                  <br>
-              <form action="#" class="p-1 bg-white" style="border-radius:20px" >
-                        <div class="col-md-12">
-                          <p class=" mb-5 text-black">Descripcion del terreno </p>
-                          <label class=" mb-1 text-black"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit
-                  aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt
-                  consequatur. Amet dolores excepturi earum unde iusto</label>
-                        </div>
-                  </form>
-               </div>
+                <form action="#" class="p-5 bg-white">
+                  <div class="col-md-12">
+                    <label class=" mb-5 text-black">CARRUSEL</label>
+                  </div>
+                </form>
+                <br>
+                <form action="#" class="p-1 bg-white" style="border-radius:20px">
+                  <div class="col-md-12">
+                    <p class=" mb-5 text-black">Descripcion del terreno </p>
+                    <label class=" mb-1 text-black"> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Consectetur unde reprehenderit
+                      aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt
+                      consequatur. Amet dolores excepturi earum unde iusto</label>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
-         <div class="container">
+      </div>
+      <div class="container">
         <div class="row align-items-left justify-content-left text-left">
           <div class="col-md-10" data-aos="fade-up" data-aos-delay="400">
             <div class="row justify-content-center mt-5">
@@ -38,125 +106,104 @@
                     <div class="col-md-7 mb-5" data-aos="fade">
                     </div>
                   </div>
-                    <h2 class="mb-1 text-white">Contactar Anunciante</h2>
-                    <form action="#" class="p-3 bg-white">
-                      <div class="row form-group">
-                        <div class="col-md-12">
-                          <label class="text-black" for="email">Correo</label>
-                          <input type="email" id="email" class="form-control">
-                        </div>
+                  <h2 class="mb-1 text-white">Contactar Anunciante</h2>
+                  <form action="#" class="p-3 bg-white">
+                    <div class="row form-group">
+                      <div class="col-md-12">
+                        <label class="text-black" for="email">Correo</label>
+                        <input type="email" id="email" class="form-control">
                       </div>
-                      
-                        <div class="row form-group">
-                          <div class="col-md-12">
-                            <label class="text-black" for="phone">Teléfono</label>
-                            <input type="text" id="phone" class="form-control">
-                          </div>
-                        </div>
-                          <div class="row form-group">
-                            <div class="col-md-12 ">
-                            <label class="text-black" for ="message"> Mensaje </label>
-                            <input type="text" id="message" class="form-control"  style="height :200px">
-                            </div>
-                          </div>
-                            <div class="row form-group">
-                              <div class="col-md-12">
-                                <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
-                              </div>
-                            </div>
-                      </form>
-                      
-                          
-                          <router-link to="/"><img src="/images/calendar.png" width="50px" height="50px"/><h2 class="text-black"> Agenda tu cita </h2></router-link>
-                         
-                          
-                         
                     </div>
-                  </div>
+                    <div class="row form-group">
+                      <div class="col-md-12">
+                        <label class="text-black" for="phone">Teléfono</label>
+                        <input type="text" id="phone" class="form-control">
+                      </div>
+                    </div>
+                    <div class="row form-group">
+                      <div class="col-md-12 ">
+                        <label class="text-black" for="message"> Mensaje </label>
+                        <input type="text" id="message" class="form-control" style="height :200px">
+                      </div>
+                    </div>
+                    <div class="row form-group">
+                      <div class="col-md-12">
+                        <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
+                      </div>
+                    </div>
+                  </form>
+                  <router-link to="/"><img src="/images/calendar.png" width="50px" height="50px" />
+                    <h2 class="text-black"> Agenda tu cita </h2>
+                  </router-link>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      
-    
-    
-
+    </div>
+  </div-->
 </template>
+
 <script>
 import PropertyListingComponent from '@/components/shared-components/PropertyListingComponent'
 
-const FEATURED_PROPERTIES = [
-  {
-    id: 13,
-    image: 'images/img_1.jpg',
-    type: 'Terreno en venta',
-    name: 'Terreno 1',
-    is_favorite: false,
-    visit_count: 18,
-    address: 'Camino a la barranca 16, Colonia, Zapotlanejo, Jalisco'
-  },
-  {
-    id: 18,
-    image: 'images/img_2.jpg',
-    type: 'Terreno en venta',
-    name: 'Terreno 2',
-    is_favorite: true,
-    visit_count: 22,
-    address: 'Camino a la barranca 16, Colonia, Zapotlanejo, Jalisco'
-  },
-  {
-    id: 26,
-    image: 'images/img_3.jpg',
-    type: 'Terreno en venta',
-    name: 'Terreno 3',
-    is_favorite: false,
-    visit_count: 26,
-    address: 'Camino a la barranca 16, Colonia, Zapotlanejo, Jalisco'
-  },
-  {
-    id: 33,
-    image: 'images/img_4.jpg',
-    type: 'Terreno en venta',
-    name: 'Terreno 4',
-    is_favorite: true,
-    visit_count: 29,
-    address: 'Camino a la barranca 16, Colonia, Zapotlanejo, Jalisco'
-  },
-  {
-    id: 35,
-    image: 'images/img_2.jpg',
-    type: 'Terreno en venta',
-    name: 'Terreno 5',
-    is_favorite: false,
-    visit_count: 64,
-    address: 'Camino a la barranca 16, Colonia, Zapotlanejo, Jalisco'
-  }
-]
 export default {
-     name: 'states-listing-component',
-     data () {
+  name: 'states-listing-component',
+  data () {
     return {
-      featuredProperties: FEATURED_PROPERTIES
+      estateImages: [
+        { src: 'images/img_1.jpg', desc: 'Fachada' },
+        { src: 'images/img_3.jpg', desc: 'Cuarto No. 1' },
+        { src: 'images/img_4.jpg', desc: 'Cochera' }
+      ]
+    }
+  },
+  mounted() {
+    console.log('Se monto este componente en el HTML');
+    this.siteCarousel();
+  },
+  methods: {
+    siteCarousel() {
+      if ( $('.nonloop-block-13').length > 0 ) {
+        $('.nonloop-block-13').owlCarousel({
+          center: false,
+          items: 1,
+          loop: true,
+          stagePadding: 5,
+          margin: 12,
+          smartSpeed: 1000,
+          autoplay: true,
+          autoplayTimeout: 1000,
+          autoplayHoverPause: false,
+          // nav: true,
+          // navText: ['<span class="icon-keyboard_arrow_left">IZQUIERDA<span>', '<span class="icon-keyboard_arrow_right">DERECHA</span>'],
+          responsive: {
+            600: {
+              margin: 30,
+              nav: true,
+              items: 2
+            },
+            1000: { 
+              margin: 30,
+              nav: true,
+              items: 3
+            },
+            1200: {
+              margin: 30,
+              nav: true,
+              items: 4
+            }
+          }
+        });
+      }
     }
   },
   components: {
-    'state-component': PropertyListingComponent
-    
-}
+    'state-component': PropertyListingComponent   
+  }
 }
 </script>
+
 <style>
-
-.cuadrado{
-padding:60px;
-width: 50%;
-margin:10px;
-border-radius: 20px;
-background-color: #ffffff;
-border: solid 1px white;
-color: white;
-}
-
 </style>
