a3bd6dae20afaf7b5e9e2066b00d8ef90336f2c0
Alan Castañeda <alancasstg@gmail.com>
2019-08-15 20:04:43 -0500

    Improvements
    FavIcon
    Header Logo color
    HomeView Contain
    StateDetails image router link
    Footer Social media links
    Dashboard background color

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,33 +1,33 @@
 <template>
   <footer class="site-footer">
     <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <div class="row">
+     
+         <div class="row align-items-center justify-content-center text-center">
             <div class="col-4">
               <h2 class="footer-heading text-black ">BYRN</h2>
               <p>Bienes y Raíces Nuño ® </p>
             </div>
 
-            <div class="col-4">
+           
+            <div class="col-md-4">
+              <h2 class="footer-heading text-black ">Enceuntranos en: </h2>
+              <a href="https://www.facebook.com/Bienes-Y-Raices-N-105244424169853/" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+              <a href="https://twitter.com/BinesByrn" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+              <a href="https://www.instagram.com/bienesbyrn/?hl=es-la" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+              
+            </div>
+             <div class="col-12">
               <h2 class="footer-heading text-black ">Contacto</h2>
               <p>
-                  byrn@gmail.com
+                  contactobyrn@gmail.com
                   <br>
                   (33) 187766 ext.12
               </p>
             </div>
-            <div class="col-4">
-              <h2 class="footer-heading text-black ">Enceuntranos en: </h2>
-              <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-              <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-              <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-              <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
-            </div>
           </div>
         </div>
 
-      </div>
+     
     </div>
   </footer>
 </template>
